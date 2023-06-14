a25f74f0f49162cffaf6f34bba13cf7bf19a23a9
Lynx010 <127880515+Lynx010@users.noreply.github.com>
2023-06-14 23:22:07 +0300

    BugFix

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public float mouseSensitivity = 2f;
-    public float cameraMinClamp, cameraMaxClamp;
+
+    [Header("Clamp Values")]
+    public float cameraMinClampX;
+    public float cameraMaxClampX;
+    /*public float cameraMinClampY;
+    public float cameraMaxClampY;*/
+
     float cameraVerticalRotation;
     
     void Start()
@@ -16,15 +22,16 @@ public class Camera : MonoBehaviour
     void Update()
     {
         //Get mouse input
-        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        float inputY = Input.GetAxis    ("Mouse Y") * mouseSensitivity;
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
-
-        //rotate around its local X axis
-        cameraVerticalRotation -= inputY;
-        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, cameraMinClamp, cameraMaxClamp);
-        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
 
         //rotate around its Y axis
         transform.Rotate(Vector3.up * inputX);
+
+        //rotate around its local X axis
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, cameraMinClampX, cameraMaxClampX);
+        //transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+        transform.localEulerAngles = new Vector3(cameraVerticalRotation, transform.localEulerAngles.y, 0f);
     }
 }
