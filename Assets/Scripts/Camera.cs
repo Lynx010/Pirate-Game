b6452f0265dbac88b11254176d0dd7a0800c1831
Lynx010 <127880515+Lynx010@users.noreply.github.com>
2023-06-14 22:51:38 +0300

    Camera Script

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public float mouseSensitivity = 2f;
+    public float cameraMinClamp, cameraMaxClamp;
+    float cameraVerticalRotation;
     
     void Start()
     {
@@ -13,7 +15,16 @@ public class Camera : MonoBehaviour
     
     void Update()
     {
+        //Get mouse input
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensitivity;
         float inputX = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        //rotate around its local X axis
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, cameraMinClamp, cameraMaxClamp);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        //rotate around its Y axis
         transform.Rotate(Vector3.up * inputX);
     }
 }
