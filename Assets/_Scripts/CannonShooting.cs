890c38857c60fd770d698f1214325f8b03b284b1
Lynx010 <127880515+Lynx010@users.noreply.github.com>
2023-06-15 12:43:05 +0300

    Script created and a few things

---
+++ Assets/_Scripts/CannonShooting.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShooting : MonoBehaviour
+{
+
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
